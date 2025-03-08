72e467df530bde3853af69348752c4a2e7de98c3
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2025-03-08 14:45:31 +0200

    login form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/"element={<Home />}/>
       <Route path="/register"element={<Register />}/>
       <Route path="/login"element={<Login />}/>
+      <Route path="/dash"element={<Dashboard />}/>
       
     </Routes>
     <Footer />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,22 @@
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <h1 className="text-center text-2xl text-blue-500 p-5">Login From</h1>
+      <div className="flex justify-center">
+        <form className="space-y-3" action="">
+        <div>
+          <label>Email</label><br />
+          <input className="border rounded-md outline-none bg-gray-50 px-3 py-1.5" type="email"placeholder="Enter Your Email" />
+        </div>
+        <div>
+          <label>Password</label><br />
+          <input className="border rounded-md outline-none bg-gray-50 px-3 py-1.5" type="password"placeholder="Enter Your Password" />
+        </div>
+        <button className="border rounded-md px-2 py-1 bg-green-400 text-white hover:bg-blue-500">Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
